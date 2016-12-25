a5e4cc583ad8d4ecce816a949b45c9caf1e2f0b0
Henning Ströker <henning_stroeker@gmx.net>
2016-12-25 17:38:32 +0100

    Dice page is working!

--- src/pages/dice/dice.ts
+++ src/pages/dice/dice.ts
@@ -8,13 +8,42 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class Dice {
 
+  unmodified: number;
+  open: number;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.dice();
+  }
+  
+  dice()
+  {
+    this.unmodified = this.throw_W100() + 1;
+    this.open = this.unmodified;
+    if (this.open <= 5)
+    {
+      this.open -= this.throw_W100_open_up();
+    }
+    if (this.open >= 96)
+    {
+      this.open += this.throw_W100_open_up();
+    }
+  }
+  
+  private throw_W100()
+  {
+    return Math.floor(Math.random() * 100); 
+  }
+  
+  private throw_W100_open_up()
+  {
+    var value = this.throw_W100();
+    var result = value;
+    while (value >= 95)
+    {
+      value = this.throw_W100();
+      result += value;
+    }
+    return result;
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-//    this.navCtrl.push(Dice, {
-//      item: item
-//    });
-  }
 }
